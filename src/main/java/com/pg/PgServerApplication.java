6747b1d0edcc5824c4d04fb7bd2f6ed3c6472788
//G0G// <ktx06155@naver.com>
2026-03-20 09:45:52 +0900

    add: initial project

---
+++ src/main/java/com/pg/PgServerApplication.java
@@ -0,0 +1,13 @@
+package com.pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgServerApplication.class, args);
+	}
+
+}
