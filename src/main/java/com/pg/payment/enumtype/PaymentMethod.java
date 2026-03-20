973e35c1d146dee4347db2a8a8036c774d233fa4
//G0G// <ktx06155@naver.com>
2026-03-20 11:53:19 +0900

    feat: add enums for payment system

---
+++ src/main/java/com/pg/payment/enumtype/PaymentMethod.java
@@ -0,0 +1,7 @@
+package com.pg.payment.enumtype;
+
+public enum PaymentMethod {
+    CARD,
+    BANK_TRANSFER,
+    VIRTUAL_ACCOUNT;
+}
---
+++ src/main/java/com/pg/payment/enumtype/TransactionType.java
@@ -0,0 +1,6 @@
+package com.pg.payment.enumtype;
+
+public enum TransactionType {
+    APPROVE,    // 승인 요청
+    CANCEL;     // 취소 요청
+}
